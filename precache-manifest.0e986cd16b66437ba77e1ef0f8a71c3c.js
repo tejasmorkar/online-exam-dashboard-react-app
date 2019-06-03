3cfd01189102ebdfaa0b81d35f8d10a247efece7
Tejas Morkar <tejasmorkar@gmail.com>
2019-06-03 15:38:11 +0530

    Updates

---
+++ precache-manifest.0e986cd16b66437ba77e1ef0f8a71c3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac1e3622c702ea93b2ba6ab1065a067",
+    "url": "/online-exam-dashboard-react-app/index.html"
+  },
+  {
+    "revision": "140ff51d84263881c02c",
+    "url": "/online-exam-dashboard-react-app/static/css/main.1ce1f0ba.chunk.css"
+  },
+  {
+    "revision": "dfad9e0b40f576c6a3fa",
+    "url": "/online-exam-dashboard-react-app/static/js/2.7d012841.chunk.js"
+  },
+  {
+    "revision": "140ff51d84263881c02c",
+    "url": "/online-exam-dashboard-react-app/static/js/main.94e30d9b.chunk.js"
+  },
+  {
+    "revision": "2d280cdedfca29e02716",
+    "url": "/online-exam-dashboard-react-app/static/js/runtime~main.9c750a4b.js"
+  }
+]);
